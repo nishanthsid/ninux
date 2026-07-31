3fab39057e8c4a67228c3a21636245ca05f86a8d
Nishanth S D <nishanthsd@zohomail.in>
2026-07-31 22:35:23 +0530

    console refactor start

--- include/video/console.h
+++ include/video/console.h
@@ -11,6 +11,8 @@ typedef struct {
     text_buffer * tb;
     uint32_t cursor_x;
     uint32_t cursor_y;
+    uint32_t scroll_offset;
+
 } console;
 
 // Deprecated - Will be removed after Console shifts to using text_buffer
@@ -18,11 +20,6 @@ void _console_put_char(char c);
 void _console_put_string(const char * string);
 void _console_put_char_color(char c, color24 fg, color24 bg);
 void _console_put_string_color(const char * string, color24 fg, color24 bg);
-
-void console_put_char(text_buffer *tb, char c);
-void console_put_string(text_buffer *tb, const char * string);
-void console_put_char_color(text_buffer *tb, char c, color24 fg, color24 bg);
-void console_put_string_color(text_buffer *tb, const char * string, color24 fg, color24 bg);
 
 
 #endif
