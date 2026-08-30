e417668537433d9cff54c822e349077205d15faf
nishanthsid <nishanthsithadhandapani@gmail.com>
2026-08-30 21:23:30 +0530

    Update gdt.c

--- src/gdt/gdt.c
+++ src/gdt/gdt.c
@@ -3,6 +3,7 @@
 // This needs to be implemented for the x86 64 long mode
 // Taking longer than expected to understand gdt
 // I am feeling a bit tired today
+// Same today 
 void set_gdt(){
     
 }
